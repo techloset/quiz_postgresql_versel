678d7e2ab8f8efb7570cc2c9102024059f8f2fa3
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-08-30 12:41:50 +0500

    add vercel URL

--- app/editQuestion/[id]/page.js
+++ app/editQuestion/[id]/page.js
@@ -3,23 +3,40 @@ import EditQuestionForm from "@/components/EditQuestionForm";
 
 const getQuestionById = async (id) =>{
 
+    // try {
+    //     const res = await fetch(`api/ques/${id}`, {cache:"no-store"})
+    //     console.log("res",res)
+    //     if(!res.ok){
+    //         throw new Error("Failed To Fetch")
+    //     }
+    //     const data = await res.json()
+    //    return data
+    // } catch (error) {
+    //     console.log(error);
+    // }
     try {
-        const res = await fetch(`https://quiz-postgresql-versel-thfl.vercel.app/api/ques/${id}`, {cache:"no-store"})
-        if(!res.ok){
-            throw new Error("Failed To Fetch")
+        const url = new URL(`/api/ques/${id}`, 'https://quiz-postgresql-versel-thfl.vercel.app');
+        
+        const res = await fetch(url, { cache: "no-store" });
+        
+        if (!res.ok) {
+            throw new Error("Failed To Fetch");
         }
-        const data = await res.json()
-       return data
+        
+        const data = await res.json();
+        return data;
     } catch (error) {
         console.log(error);
     }
     
+    
 }
 
 export default async function EditQuestion({params}) {
-
+  console.log("fjasdfajsdl",params)
     const {id} = params
     const {questions} = await getQuestionById(id)
+    console.log("first",questions)
     const {question, option1,option2,option3,correctOption} = questions 
 
   return (
