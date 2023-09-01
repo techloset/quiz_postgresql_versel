94b9ea5fe6768cae09824efa8dcee0013dcaab16
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-01 16:42:12 +0500

    responsive

--- components/UserQuestions.js
+++ components/UserQuestions.js
@@ -31,47 +31,50 @@ export default function UserQuestions() {
             <h1 className="text-center font-bold text-xl mt-10"> Questions You can Edit or Delete</h1>
             {state.length > 0 ? (
                 state.map((q, i) => (
-                    <div key={i} className="w-[100vh] mt-16  mx-auto">
-                        <div className="bg-gray-100 p-11 rounded-lg shadow-xl">
-                            <div className="m-3 font-bold text-xl"></div>
-                            <h2 className="text-2xl font-semibold mb-2">Q {i + 1}: {q.question}</h2>
-                            <div className="space-y-2 text-lg">
-                                <label className="flex items-center">
-                                    <input
-                                        type="radio"
-                                        name={q._id}
-                                        value={q.option1}
-                                        className="mr-2"
-                                    />
-                                    {q.option1}
-                                </label>
-                                <label className="flex items-center">
-                                    <input
-                                        type="radio"
-                                        name={q._id}
-                                        value={q.option2}
-                                        className="mr-2"
-                                    />
-                                    {q.option2}
-                                </label>
-                                <label className="flex items-center">
-                                    <input
-                                        type="radio"
-                                        name={q._id}
-                                        value={q.option3}
-                                        className="mr-2"
-                                    />
-                                    {q.option3}
-                                </label>
-                                <div className="text-end">
-                                    <Link href={`/editQuestion/${q.id}`} className="py-2 px-2 bg-blue-400 mx-2 text-white rounded-md">
-                                        Edit
-                                    </Link>
-                                    <RemoveBtn id={q.id} />
-                                </div>
+                    <div key={i} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mt-4 sm:mt-8 md:mt-16 mx-auto">
+                    <div className="bg-gray-100 p-4 md:p-6 lg:p-8 rounded-lg shadow-xl">
+                        <div className="m-3 font-bold text-lg md:text-xl lg:text-2xl"></div>
+                        <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-2">
+                            Q {i + 1}: {q.question}
+                        </h2>
+                        <div className="space-y-2 text-base md:text-lg lg:text-xl">
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name={q._id}
+                                    value={q.option1}
+                                    className="mr-2"
+                                />
+                                {q.option1}
+                            </label>
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name={q._id}
+                                    value={q.option2}
+                                    className="mr-2"
+                                />
+                                {q.option2}
+                            </label>
+                            <label className="flex items-center">
+                                <input
+                                    type="radio"
+                                    name={q._id}
+                                    value={q.option3}
+                                    className="mr-2"
+                                />
+                                {q.option3}
+                            </label>
+                            <div className="text-end">
+                                <Link href={`/editQuestion/${q.id}`} className="py-2 px-2 bg-blue-400 mx-2 text-white rounded-md">
+                                    Edit
+                                </Link>
+                                <RemoveBtn id={q.id} />
                             </div>
                         </div>
                     </div>
+                </div>
+                
                 ))
             ) : (
 
