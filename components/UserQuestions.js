542936e8fc4705906efc682a138e7d106e32e514
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-01 16:46:13 +0500

    updated

--- components/UserQuestions.js
+++ components/UserQuestions.js
@@ -31,7 +31,7 @@ export default function UserQuestions() {
             <h1 className="text-center font-bold text-xl mt-10"> Questions You can Edit or Delete</h1>
             {state.length > 0 ? (
                 state.map((q, i) => (
-                    <div key={i} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mt-4 sm:mt-8 md:mt-16 mx-auto">
+                    <div key={i} className="w-full sm:w-1/2 md:w-1/3 lg:w-11/12 xl:w-1/5 mt-4 sm:mt-8 md:mt-16 mx-auto">
                     <div className="bg-gray-100 p-4 md:p-6 lg:p-8 rounded-lg shadow-xl">
                         <div className="m-3 font-bold text-lg md:text-xl lg:text-2xl"></div>
                         <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold mb-2">
