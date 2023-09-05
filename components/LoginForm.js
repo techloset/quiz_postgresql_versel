1d22b229b6b2ce43fb9fb2f3e88dd86929d8d18f
Muhammad Ahmed <ahmadginideveloper@gmail.com>
2023-09-05 19:11:50 +0500

    fix QAA pointd

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -31,7 +31,6 @@ export default function LoginForm() {
         }
       router.push("/quiz");
       setisLoading(false)
-      window.location.reload()
       
     } catch (error) {
       console.log(error);
